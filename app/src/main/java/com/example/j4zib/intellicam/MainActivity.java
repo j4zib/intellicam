b7d379f134cb4d1fb26a6f389fc67fad0c48fedf
zeeshanhussain <zeeshanhussain12@gmail.com>
2019-01-08 21:17:23 +0530

    add push notifications

--- app/src/main/java/com/example/j4zib/intellicam/MainActivity.java
+++ app/src/main/java/com/example/j4zib/intellicam/MainActivity.java
@@ -23,6 +23,7 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FirebaseMessaging.getInstance().subscribeToTopic("pushNotifications");
         Query query = notebookRef;
 
         FirestoreRecyclerOptions<Person> options = new FirestoreRecyclerOptions.Builder<Person>()
---
+++ app/src/main/java/com/example/j4zib/intellicam/MyFirebaseMessagingService.java
@@ -0,0 +1,55 @@
+package com.example.j4zib.intellicam;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    public MyFirebaseMessagingService() {
+    }
+
+    private static final String TAG = "FirebaseMessagingServce";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        String notificationTitle = null, notificationBody = null;
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size()>0) {
+            notificationTitle = remoteMessage.getData().get("name");
+            notificationBody = remoteMessage.getData().get("spam");
+        }
+
+        sendNotification(notificationTitle, notificationBody);
+    }
+
+
+    private void sendNotification(String notificationTitle, String notificationBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setAutoCancel(true)   //Automatically delete the notification
+                .setSmallIcon(R.mipmap.ic_launcher) //Notification icon
+                .setContentIntent(pendingIntent)
+                .setContentTitle(notificationTitle)
+                .setContentText(notificationBody)
+                .setSound(defaultSoundUri);
+
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
